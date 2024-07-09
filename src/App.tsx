d01f1ae842e7ea30b5b7460c3d094113563ac976
Sensei Stiven <strujillodev@gmail.com>
2024-07-09 05:42:40 -0500

    feat: create Activity List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useReducer } from 'react';
-import Form from './components/Form';
-import { activityReducer, initialState } from './reducers/activityReducer';
+import { useReducer } from "react";
+import ActivityList from './components/ActivityList';
+import Form from "./components/Form";
+import { activityReducer, initialState } from "./reducers/activityReducer";
 
 function App() {
 
@@ -20,13 +21,15 @@ function App() {
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form
-            dispatch={dispatch}
-          />
+          <Form dispatch={dispatch} />
         </div>
       </section>
+
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList activities={state.activities}/>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
